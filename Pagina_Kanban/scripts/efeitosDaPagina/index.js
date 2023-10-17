56aabaf788310f1d1d093d27f688feb6440f1b82
Dario <dariokleinalves@gmail.com>
2023-10-17 12:32:03 -0300

    adicionado botão de edit

--- Pagina_Kanban/scripts/efeitosDaPagina/index.js
+++ Pagina_Kanban/scripts/efeitosDaPagina/index.js
@@ -1,24 +1,49 @@
-const [btnAfazer, btnEmAndamento, btnConcluido] = document.querySelectorAll(".buttonAddTask")
+const main = document.getElementById("containerAllTasks")
+main.style.display = "none"
+setTimeout(() => {
+    main.style.display = "flex"
+}, 1500)
+
+const [btnAfazer, btnEmAndamento, btnConcluido] =
+  document.querySelectorAll(".buttonAddTask")
 
 const containerForms = document.getElementById("containerForms")
-const body = document.getElementsByTagName('body')[0]
+const containerEntries = document.getElementById("containerEntries")
+const editTasksForm = document.getElementById("editTasksForm")
+const body = document.getElementsByTagName("body")[0]
 
-btnAfazer.addEventListener('click', () => {
+btnAfazer.addEventListener("click", () => {
   containerForms.style.display = "flex"
-  body.style.overflow = 'hidden'
+  body.style.overflow = "hidden"
+  containerEntries.style.display = "flex"
 })
-btnEmAndamento.addEventListener('click', () => {
+btnEmAndamento.addEventListener("click", () => {
   containerForms.style.display = "flex"
-  body.style.overflow = 'hidden'
+  body.style.overflow = "hidden"
+  containerEntries.style.display = "flex"
 })
-btnConcluido.addEventListener('click', () => {
+btnConcluido.addEventListener("click", () => {
   containerForms.style.display = "flex"
-  body.style.overflow = 'hidden'
+  body.style.overflow = "hidden"
+  containerEntries.style.display = "flex"
 })
 
-const cancelTask = document.getElementById("cancelTask")
+const cancelTask = document.querySelectorAll(".cancelTask")
 
-cancelTask.addEventListener('click', () => {
-  containerForms.style.display = "none"
-  body.style.overflow = 'scroll'
+cancelTask.forEach((button) => {
+  button.addEventListener("click", () => {
+    containerEntries.style.display = "none"
+    editTasksForm.style.display = "none"
+    containerForms.style.display = "none"
+    body.style.overflow = "scroll"
+  })
+})
+
+const editButton = document.querySelectorAll(".edit-button")
+editButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    containerForms.style.display = "flex"
+    editTasksForm.style.display = "flex"
+    body.style.overflow = "hidden"
+  })
 })
