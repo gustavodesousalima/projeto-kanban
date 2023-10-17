07db94b9589db35cb2fc3424a788dd456eda9e9c
Gustavo de Sousa Lima <Gustavdesousalima@gmail.com>
2023-10-17 04:45:41 -0300

    Adicionando dimensões responsivas

--- Pagina_Kanban/scripts/efeitosDaPagina/index.js
+++ Pagina_Kanban/scripts/efeitosDaPagina/index.js
@@ -1,19 +1,24 @@
 const [btnAfazer, btnEmAndamento, btnConcluido] = document.querySelectorAll(".buttonAddTask")
 
 const containerForms = document.getElementById("containerForms")
+const body = document.getElementsByTagName('body')[0]
 
-btnAfazer.addEventListener('click', ()=> {
+btnAfazer.addEventListener('click', () => {
   containerForms.style.display = "flex"
+  body.style.overflow = 'hidden'
 })
-btnEmAndamento.addEventListener('click', ()=> {
+btnEmAndamento.addEventListener('click', () => {
   containerForms.style.display = "flex"
+  body.style.overflow = 'hidden'
 })
-btnConcluido.addEventListener('click', ()=> {
+btnConcluido.addEventListener('click', () => {
   containerForms.style.display = "flex"
+  body.style.overflow = 'hidden'
 })
 
 const cancelTask = document.getElementById("cancelTask")
 
-cancelTask.addEventListener('click', ()=> {
+cancelTask.addEventListener('click', () => {
   containerForms.style.display = "none"
+  body.style.overflow = 'scroll'
 })
