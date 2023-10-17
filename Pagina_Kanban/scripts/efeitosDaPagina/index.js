cd28880fe1a0193ff81380eeeb0280f5530d1680
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-10-17 11:22:21 -0300

    implementação da função criar tarefa

--- Pagina_Kanban/scripts/efeitosDaPagina/index.js
+++ Pagina_Kanban/scripts/efeitosDaPagina/index.js
@@ -1,19 +1,24 @@
-const [btnAfazer, btnEmAndamento, btnConcluido] = document.querySelectorAll(".buttonAddTask")
-
+/* const [btnAfazer, btnEmAndamento, btnConcluido] = document.querySelectorAll(".buttonAddTask")
+export let botaoStatusTarefa
 const containerForms = document.getElementById("containerForms")
 
 btnAfazer.addEventListener('click', ()=> {
   containerForms.style.display = "flex"
+  botaoStatusTarefa = 'btnAfazer'
 })
 btnEmAndamento.addEventListener('click', ()=> {
   containerForms.style.display = "flex"
+  botaoStatusTarefa = 'btnEmAndamento'
 })
 btnConcluido.addEventListener('click', ()=> {
   containerForms.style.display = "flex"
+  botaoStatusTarefa = 'btnConcluido'
 })
 
 const cancelTask = document.getElementById("cancelTask")
 
 cancelTask.addEventListener('click', ()=> {
   containerForms.style.display = "none"
-})
+}) */
+
+
